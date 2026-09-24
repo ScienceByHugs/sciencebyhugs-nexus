d0d9cce90cbfce81d774bba20e99c5741381da3a
Science by hugs <anthony@sciencebyhugs.com>
2026-09-24 09:09:23 -0700

    Use stable Nexus asset filenames

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ const base = process.env.VITE_BASE_PATH || '/'
 
 export default defineConfig({
   base,
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
   plugins: [
     VitePWA({
       registerType: 'prompt',
